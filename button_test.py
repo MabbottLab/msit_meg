83ad4954ba4a610f5c59f09f6a2a05a8337b221e
Julie Tseng <julie.tseng@sickkids.ca>
2021-11-29 13:55:09 -0500

    readData() directly instead of unnecessary buttons. instantiate once and use global variable

--- button_test.py
+++ button_test.py
@@ -1,54 +1,16 @@
-from psychopy import parallel, event
+from psychopy import parallel
 
-# CLOCK
-key_resp = event.BuilderKeyResponse()
+# BUTTON BOX 
+BBOX = parallel.ParallelPort(0x3048) # instantiate the port
 
-# BUTTON BOX RESPONSES----------------------------------------------------#
-BBOX_1_OR_2 = parallel.ParallelPort(0x3048)
-BBOX_3 = parallel.ParallelPort(0x3048+2)
+# flip the control bit, if necessary
+ctrl_port = parallel.ParallelPort(0x3048+2)
+ctrl_port.setPin(7, 1)
 
-# return button press
-def readButtons():
-    if BBOX_1_OR_2.readPin(2):
-        return('1')
-    elif BBOX_1_OR_2.readPin(3):
-        return('2')
-    elif BBOX_3.readPin(2):
-        return('3')
-    else:
-        return('none')
-
-# SENDING OUT info about trials--------------------------------------------#
-MEG_ACQ         = parallel.ParallelPort(address=0x4048)
-triplet_congruent     = 1
-triplet_incongruent   = 2
-button_out            = [4, 8, 16]
-
-# send signal to ACQ
-def sendTrigger(triggerVal):
-    MEG_ACQ.setData(int(triggerVal))
-    core.wait(0.01)
-    MEG_ACQ.setData(0)
+num_times_pressed = 0 # test for 
+while num_times_pressed < 20:
+    response = BBOX.readData() # returns 1, 2, 4
     
-pressed = False
-
-key_resp.keys = []
-key_resp.rt = []
-key_resp.clock.reset()
-
-num_times_pressed = 0
-while not pressed:
-    response = readButtons()
-    
-    if response != 'none':
-        key_resp.rt = key_resp.clock.getTime()
-        sendTrigger(button_out[int(response)-1])
-        key_resp.keys = response
-        print("Button press detected!\nReaction Time: %.2f" % key_resp.rt)
+    if response: # only proceeds if response = 1, 2, 4, breaks if response = 0
+        print(response)
         num_times_pressed += 1
-        
-        key_resp.rt = []
-        key_resp.clock.reset()
-        
-    if num_times_pressed > 5:
-        pressed = True
